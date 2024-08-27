3607d880d10f212e3b4e6674cea870a5ee129fb7
Parjanya Mudunuri <137906335+parj-hsbc@users.noreply.github.com>
2024-08-27 13:54:13 +0100

    Added postgres as a test container (#77)
    * Added postgres test container
    
    * Added support for snapshot build
    
    * Removed redundant bits
    
    * Added 21, removed dependency
    
    * Fix Java 21 doc issue
    
    * Tidy up actions

--- src/main/java/com/hsbc/engineering/JDBCInquirer.java
+++ src/main/java/com/hsbc/engineering/JDBCInquirer.java
@@ -7,7 +7,6 @@ import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
 import java.util.logging.Logger;
@@ -50,6 +49,12 @@ import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPacka
  *
  */
 public class JDBCInquirer {
+
+    /**
+     * EmptyContructor to fix issue with Java 21 asking for java doc - warning: use of default constructor, which does not provide a comment
+     */
+    public JDBCInquirer() { }
+    
     /**
      * Default logger
      */
